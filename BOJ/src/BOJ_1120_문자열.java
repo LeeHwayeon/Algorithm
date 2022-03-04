e10beb6c7448d0998eb4927b29920880979a0ca5
Hwayeon <snw4002@hanmail.net>
2022-03-04 19:33:45 +0900

    update : BOJ_1120

--- BOJ/src/BOJ_1120_문자열.java
+++ BOJ/src/BOJ_1120_문자열.java
@@ -14,31 +14,17 @@ public class BOJ_1120_문자열 {
 		int aLen = A.length();
 		int bLen = B.length();
 
-		int cnt = 0; // 차이 횟수
-		if (aLen == bLen) { // 길이가 같으면 A 앞,뒤에 알파벳 추가할 수 없기 때문에 바로 비교
-			for (int i = 0; i < aLen; i++) {
-				if (A.charAt(i) != B.charAt(i)) {
+		int min = 50; // 최대 50글자
+		for (int i = 0; i < (bLen - aLen) + 1; i++) { // A와 B길이의 차의 +1만큼 반복
+			int cnt = 0; // 차이 횟수
+			for (int j = 0; j < aLen; j++) { // B는 비교 시작위치를 한칸씩 옮겨야 함
+				if (A.charAt(j) != B.charAt(j + i)) {
 					cnt++;
 				}
 			}
-		} else {
-			if (B.contains(A)) { // 만약 B에 A문자가 포함되어 있으면 똑같은 알파벳을 앞뒤로 추가해주면 되기 때문에 횟수 0
-				cnt = 0;
-			} else {
-				int min = 50; // 최대 50글자
-				for (int i = 0; i < (bLen - aLen) + 1; i++) { // A와 B길이의 차의 +1만큼 반복
-					cnt = 0;
-					for (int j = 0; j < aLen; j++) { // B는 비교 시작위치를 한칸씩 옮겨야 함
-						if (A.charAt(j) != B.charAt(j + i)) {
-							cnt++;
-						}
-					}
-					min = Math.min(min, cnt);
-				}
-				cnt = min;
-			}
+			min = Math.min(min, cnt);
 		}
-		System.out.println(cnt);
+		System.out.println(min);
 	}
 
 }
